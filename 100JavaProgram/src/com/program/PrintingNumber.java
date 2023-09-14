402090cea80cc1fd754653c5d14f0aafcf30a8f7
visavambhar <visavambhar4643@gmail.com>
2023-09-14 22:17:31 +0530

    first commi

---
+++ 100JavaProgram/src/com/program/PrintingNumber.java
@@ -0,0 +1,15 @@
+package com.program;
+
+public class PrintingNumber {
+	public static void main(String[] args) {
+		
+		/*
+		 * for(int r=1;r<11;r++) { System.out.println(r); }
+		 */
+		for(String t:args)
+		{
+			System.out.println(t);
+		}
+	}
+
+}
---
+++ 100JavaProgram/src/com/program/PrintingOutput.java
@@ -0,0 +1,16 @@
+package com.program;
+
+public class PrintingOutput {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi buddy how are you? I am in main method");
+		print();
+	}
+	
+	static void print()
+	{
+		System.out.println("I am in print method");
+	}
+
+}
